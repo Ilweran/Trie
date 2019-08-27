f3325fd88d0c4fac0f6071c7128ac2c8ed358c5c
Ilweran <quizblow@gmail.com>
2019-08-27 07:56:47 +0400

    Writing depth first search algorithm

--- Trie.cpp
+++ Trie.cpp
@@ -21,7 +21,7 @@ Trie::Trie()
 }
 
 // Iterative function to insert a key in the Trie
-void Trie::insertString(string key)
+void Trie::insertString(string str)
 {
   //Check if Trie is empty and create the top TrieNode if it is
   if ( rootPtr == nullptr )
@@ -29,64 +29,69 @@ void Trie::insertString(string key)
 
 	// Now when Trie is not empty let's start inserting a string from root node
 	TrieNode* curr = rootPtr;
-	for (unsigned short int i = 0; i < key.length(); i++)
+	for (unsigned short int i = 0; i < str.length(); i++)
 	{
 		// create a new node if path doesn't exist
-		if (curr->TrieNode::letter[key[i]] == nullptr)
-			curr->TrieNode::letter[key[i]] = new TrieNode();
+		if (curr->TrieNode::letter[str[i]] == nullptr)
+			curr->TrieNode::letter[str[i]] = new TrieNode();
 		// go to next node
-  	curr = curr->TrieNode::letter[key[i]];
+  	curr = curr->TrieNode::letter[str[i]];
 	}
-
 	// mark current node as leaf
 	curr->TrieNode::setIsLeaf();
+  if ( longestWord < str.size() )
+    longestWord = str.size();
 }
 
 // Iterative function to search a key in Trie. It returns true
-// if the key is found in the Trie, else it returns false
-bool Trie::searchString(string key)
+// if the key is found in the Trie, otherwise returns false
+bool Trie::searchString(string str)
 {
 	// return false if Trie is empty
 	if (rootPtr == nullptr)
 		return false;
 
 	TrieNode* curr = rootPtr;
-	for (unsigned short int i = 0; i < key.length(); i++)
+	for (unsigned short int i = 0; i < str.length(); i++)
 	{
 		// go to next node
-		curr = curr->TrieNode::letter[key[i]];
+		curr = curr->TrieNode::letter[str[i]];
 
 		// if string is invalid (reached end of path in Trie)
 		if (curr == nullptr)
 			return false;
 	}
 	// if current node is a leaf then we have reached the
-	// end of the string, set markPtr and return true
+	// end of the string, set markPtr, check and set longestWord and return true
 	if ( curr->TrieNode::getIsLeaf() )
 	{
 	  markPtr = curr;
-	  return true;
+	  if ( longestWord < str.size() )
+	    longestWord = str.size();
 	}
+	return true;
 }
 
-// returns true if given node has any children
-bool Trie::haveChildren(TrieNode * curr)
+// Returns true if given node has any children
+bool Trie::haveChildren(TrieNode * curr) const
 {
 	for (unsigned short int i = 0; i < ALPHABET_SIZE; i++)
 		if (curr->getNextNode(i) != nullptr)
-			return true;	// A child found
+			return true;	// A child found :)
 	return false;     //Childless :(
 }
 
 bool Trie::deleteNode(string prefix)
 {
-  if ( Trie::rootPtr == nullptr )
+  if ( rootPtr == nullptr )
     return false;
   if (Trie::searchString(prefix))
   {
-    delete Trie::markPtr;
-    Trie::markPtr = nullptr;
+    delete markPtr;
+    markPtr = nullptr;
   }
+  if ( longestWord == prefix.size() ) //If we are deleting a node for the longest word in the Trie
+    longestWord--;                    //Adjust longestWord
   return true;
 }
 
@@ -102,7 +107,6 @@ bool Trie::deleteString(TrieNode * curr, string str)
 	{
 		// Recur for the node corresponding to next character in the key
 		// and if it returns true, delete current node (if it is non-leaf)
-
 		if (curr != nullptr &&
 			curr->TrieNode::getNextNode(str[0]) != nullptr &&
 			deleteString(curr->getNextNode(str[0]), str.substr(1)) &&
@@ -119,12 +123,11 @@ bool Trie::deleteString(TrieNode * curr, string str)
 			}
 		}
 	}
-
 	// if we have reached the end of the key
 	if (str.length() == 0 && curr->getIsLeaf())
 	{
 		// if current node is a leaf node and don't have any children
-		if (!Trie::haveChildren(curr))
+		if (!haveChildren(curr))
 		{
 			// delete current node
 			delete curr;
@@ -133,17 +136,32 @@ bool Trie::deleteString(TrieNode * curr, string str)
 			// delete non-leaf parent nodes
 			return true;
 		}
-
 		// if current node is a leaf node and have children
 		else
 		{
 			// mark current node as non-leaf node (DON'T DELETE IT)
 			curr->setIsLeaf(false);
-
 			// don't delete its parent nodes
 			return false;
 		}
 	}
 
 	return false;
+}
+
+void Trie::dfs() const
+{
+  if (rootPtr == nullptr)
+    return;
+  for(unsigned short int i = 0; i < ALPHABET_SIZE; i++)
+  {
+    if ( TrieNode::letter[i] != nullptr )
+      word += (char)i;
+  }
+}
+
+int Trie::searchLongestWord() const
+{
+  //dfs() comes here
+  return longestWord;
 }
--- Trie.h
+++ Trie.h
@@ -20,7 +20,8 @@ public:
   ~Trie();
   void insertString(string str);
   bool searchString(string str);
-  bool haveChildren(TrieNode * curr);
+  int searchLongestWord() const;
+  bool haveChildren(TrieNode * curr) const;
   bool deleteNode(string str);
   bool deleteString(TrieNode * curr, string str);
   void dfs() const;
@@ -29,10 +30,14 @@ private:
   TrieNode *lastPtr;  //Points to the last TrieNode being pointed to by all leaf TrieNodes
   TrieNode *markPtr;  //A marker that is used to point to found TrieNode
                       //which is set to nullptr otherwise
-  unsigned int longestWord; //:
+  unsigned short int longestWord; //:
   /*This is tracked by the Trie during inserting each new word into itself
   so that later when preparing to print words from itself it could allocate enough array
   indices to address all required characters of a word including the longest one that it contains*/
+  string word;  //:
+  /*This variable is used in depth first search Trie traversal algorithm to store detected string sequences
+  of key elements present in the Trie
+  */
 };
 
 #endif /* TRIE_H_ */
